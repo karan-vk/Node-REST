1d571040c6d7446d00e3f5c4e62d61c16d48a2cf
Karan V <karanvknarayanan@gmail.com>
2020-09-30 15:34:43 +0530

    first commit

---
+++ api/routes/products.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const router = express.Router();
+router.get("/", (req, res, next) => {
+  res.status(200).json({
+    message: "Handling GET requests to / product",
+  });
+});
---
+++ app.js
@@ -0,0 +1,6 @@
+const express = require("express");
+const app = express();
+const productRoutes = require("./api/routes/products");
+app.use("/products", productRoutes);
+
+module.exports = app;
