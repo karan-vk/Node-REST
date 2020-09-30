af5493dce9e462b05dca0bd18121cc71eb61862e
Karan V <karanvknarayanan@gmail.com>
2020-09-30 21:24:14 +0530

    product implemetaion fix

--- api/routes/products.js
+++ api/routes/products.js
@@ -8,7 +8,7 @@ router.get("/", (req, res, next) => {
     .exec()
     .then((docs) => {
       console.log(docs);
-      if (docs.length >= 10) {
+      if (docs.length >= 0) {
         res.status(200).json(docs);
       } else {
         res.status(404).json({
