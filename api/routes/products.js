ea4a09bc68841762d93cebe25fdc389ecd630db7
Karan V <karanvknarayanan@gmail.com>
2020-09-30 23:19:44 +0530

    orders completed

--- api/routes/products.js
+++ api/routes/products.js
@@ -5,16 +5,23 @@ const mongoose = require("mongoose");
 
 router.get("/", (req, res, next) => {
   Product.find()
+    .select("name price _id")
     .exec()
     .then((docs) => {
-      console.log(docs);
-      if (docs.length >= 0) {
-        res.status(200).json(docs);
-      } else {
-        res.status(404).json({
-          message: "No entries found",
-        });
-      }
+      const response = {
+        count: docs.length,
+        products: docs.map((doc) => {
+          return {
+            ...doc._doc,
+            request: {
+              type: "GET",
+              url: "http://localhost:3000/products/" + doc._id,
+            },
+          };
+        }),
+      };
+
+      res.status(200).json(response);
     })
     .catch((err) => {
       console.log(err);
@@ -32,8 +39,16 @@ router.post("/", (req, res, next) => {
     .then((result) => {
       console.log(result);
       res.status(201).json({
-        message: "Handling POST requests to / product",
-        createdProduct: product,
+        message: "Created Product successfully",
+        createdProduct: {
+          name: result.name,
+          price: result.price,
+          _id: result._id,
+          request: {
+            type: "GET",
+            url: "http://localhost:3000/products/" + doc._id,
+          },
+        },
       });
     })
     .catch((err) => {
@@ -45,10 +60,17 @@ router.post("/", (req, res, next) => {
 router.get("/:productId", (req, res, next) => {
   const id = req.params.productId;
   Product.findById(id)
+    .select("_id name price")
     .then((doc) => {
       console.log(doc);
       if (doc) {
-        res.status(200).json(doc);
+        res.status(200).json({
+          product: doc,
+          request: {
+            type: "GET",
+            url: "http://localhost:3000/products",
+          },
+        });
       } else {
         res
           .status(404)
@@ -70,8 +92,13 @@ router.patch("/:productId", (req, res, next) => {
   Product.update({ _id: id }, { $set: updateOps })
     .exec()
     .then((result) => {
-      console.log(result);
-      res.status(200).json(result);
+      res.status(200).json({
+        message: "Product updated",
+        request: {
+          type: "GET",
+          url: "http://localhost:3000/products" + id,
+        },
+      });
     })
     .catch((err) => {
       console.log(err);
